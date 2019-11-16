454a324795fb99a833e6c3e521fa9f6e6fb3bc3f
stellie82 <kims82@gmail.com>
2019-11-16 12:43:05 -0800

    setup express and add html pages

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+
+var PORT = 3000;
+var app = express();
+
+var waitlist = [];
+// Create an array to hold reservations
+var reservations = [];
+
+// Set up express to handle parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/api/reserve", function (req,res) {
+    return res.json(reservations);
+})
+
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.post("/api/reserve", function (req, res) {
+    var newReservation = req.body;
+    console.log(newReservation);
+    reservations.push(newReservation);
+    res.json(newReservation);
+});
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
